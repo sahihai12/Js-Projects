1e3a3aba862d5cef4a93746d3bb217b7c1a77db2
sahihai12 <skp28195@gmail.com>
2022-06-13 00:28:15 +0530

    changes done

--- projects/9weatherApp/main.js
+++ projects/9weatherApp/main.js
@@ -8,7 +8,7 @@ document.querySelector('.location').addEventListener('click' , ()=>{
     const geolocation = navigator.geolocation.getCurrentPosition(showposition);
 });
 
-function showposition(locationdata) {
+async function showposition(locationdata) {
     console.log(locationdata);
     let latitude = locationdata.coords.latitude;
     let longitude = locationdata.coords.longitude;
@@ -16,21 +16,23 @@ function showposition(locationdata) {
     
     fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=de5f3c726f96d8d5af62ae5017f72c99`).then(response =>  response.json()).then(json => showdata(json)).catch(e => console.log(e))
 }
-// https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+// https://api.openweathermap.org/data/2.5/weather?q={cityname}&appid={API key}
  
 
 // showdata in html
 function showdata(data) {
     console.log(data);
     city.textContent =  data.name;
-    getday();
     humidity.textContent = data.main.humidity + '%';
     wind.textContent = data.wind.speed + 'km/h';
 
 }
  
+// image from icon
+//         var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
 
-const getday = () =>  {
+// http://openweathermap.org/img/w/50n.png
+const getdayDate = () =>  {
     const weekday = [
         "Sunday",
         "Monday",
@@ -41,6 +43,15 @@ const getday = () =>  {
         "Saturday",
     ];
     const d = new Date();
+    let date = d.getDate();
+    let month = d.getMonth();
+    let year = d.getFullYear();
+    console.log(date);
+    console.log(month);
+    console.log(year);
     let day = weekday[d.getDay()];
     document.querySelector('.day').textContent = day;
 }
+
+getdayDate();
+
